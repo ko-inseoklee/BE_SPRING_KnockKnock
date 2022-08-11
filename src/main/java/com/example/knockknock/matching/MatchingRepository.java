3bb20bdb64349e173e46ef5982460b3c2c6fcb85
mek-ics-inseoklee <mek.ics.inseoklee@gmail.com>
2022-08-11 16:50:46 +0900

    Matching API Structure 개발중.

---
+++ src/main/java/com/example/knockknock/matching/MatchingRepository.java
@@ -0,0 +1,6 @@
+package com.example.knockknock.matching;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MatchingRepository extends JpaRepository<MatchingVO, Integer> {
+}
