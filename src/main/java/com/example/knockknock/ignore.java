730bd59dc961cbc638832e9e5305406a52517fc9
Lee In Seok <tjrkd222@gmail.com>
2022-10-07 12:47:25 +0900

    why not

---
+++ src/main/java/com/example/knockknock/ignore.java
@@ -0,0 +1,6 @@
+package com.example.knockknock;
+
+public class ignore {
+    public static String SMS_API_KEY = "NCSKJ7AATDUXFBNR";
+    public static String SMS_API_SECRET_KEY = "LEBBM0ENWVQ6FT7QN3HFHS4IBYAX6ELD";
+}
